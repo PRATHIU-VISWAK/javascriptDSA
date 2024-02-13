79fecbf6e3050940c914e00b05b40e250c4e91f3
PRATHIUVISWAK <prathiuviswakdev@gmail.com>
2024-02-13 10:45:15 +0530

    v2

--- traversy-js-challenges/02-basic-challenges-2/05-find-missing-letter/find-missing-letter.js
+++ traversy-js-challenges/02-basic-challenges-2/05-find-missing-letter/find-missing-letter.js
@@ -1,4 +1,11 @@
 function findMissingLetter() {
+    len = arr.length
+    let arr2=[]
+    for(let x=0;x<=len;x++){
+        arr2[x]=x+1
+    }
+    x=0
+    
     for(let i=0;i<=len;i++){
         for(let j=0;j<=len;j++){
             if(arr[i]==arr[j]){
@@ -8,6 +15,12 @@ function findMissingLetter() {
                     }
                 }
             }
+            
+        }
+    }
+    for( x=0;x<=len;x++){
+        if(arr2[x]!=0){
+            return arr2[x]
         }
     }
             
